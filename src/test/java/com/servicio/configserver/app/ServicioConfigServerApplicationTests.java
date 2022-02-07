49993e0135fc459dbe21ae8fc468ff12b5adfd1f
Christian <christian.devsw@gmail.com>
2022-02-06 20:05:50 -0500

    ultimo commit config server

---
+++ src/test/java/com/servicio/configserver/app/ServicioConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servicio.configserver.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
